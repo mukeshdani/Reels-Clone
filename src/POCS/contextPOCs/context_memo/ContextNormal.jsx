940ddd230933e5fd79bec5ae1e578753be86b8d4
Mukesh Dani <mukeshdani00@gmail.com>
2022-05-12 12:57:43 +0530

    React Basics

---
+++ src/POCS/contextPOCs/context_memo/ContextNormal.jsx
@@ -0,0 +1,21 @@
+import React, { useContext, useState } from 'react';
+// same message that will not change in near future 
+import Parent from "./Parent";
+export let Context = React.createContext("Hello");
+
+export default function ContextNormal() {
+    const [message, setMessage] = useState("fake message");
+    const changeMessage = () => {
+        setMessage("Message updated");
+    }
+    console.log("rendered context Normal")
+    return (<>
+        <div>contextNormal</div>
+        <button onClick={changeMessage}>Click Button</button>
+        <Context.Provider value={message}>
+            <Parent></Parent>
+        </Context.Provider>
+    </>
+    )
+}
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 // import UseffectDEMO from './UseffectDEMO';
-import Context from './contextPOCs/Context';
-import ThemeManager from "./contextPOCs/ThemeChanger";
-
+// import Context from './contextPOCs/theme/Context';
+// import ThemeManager from "./contextPOCs/ThemeChanger";
+import { BrowserRouter } from "react-router-dom";
+// import ContextNormal from './contextPOCs/context_memo/ContextNormal';
+import UseState from "./POCS/useState/UseState";
+import UseReducer from "./POCS/useState/useReducer"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <App />
+    // <BrowserRouter>
+    //     <App />
+    // </BrowserRouter>
+    // useState
+    // <UseState></UseState>
+    <UseReducer></UseReducer>
+
+    // <ContextNormal></ContextNormal>    
     // <UseffectDEMO></UseffectDEMO>
     // <Context />
-    <ThemeManager></ThemeManager>
+    // <ThemeManager></ThemeManager>
 );
