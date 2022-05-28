b6ac4cd07adcc2b6d6bc5442e52a63268f44d010
Mukesh Dani <mukeshdani00@gmail.com>
2022-05-29 00:25:28 +0530

    Added upto Sample Login

--- reelsclone/src/components/Login.jsx
+++ reelsclone/src/components/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React , { useState } from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+    const trackEmail = function (e) {
+        setEmail(e.target.value);
+      }
+      const trackPassword = function (e) {
+        setPassword(e.target.value);
+      }
+    const printDetails = (e)=>{
+      alert(email + " " + password);
+    }
+    return (
+   <>
+      <input type="email" onChange={trackEmail} value={email} placeholder="email" ></input>
+        <br></br>
+      <input type="password" onChange={trackPassword} value={password} placeholder="password"></input>
+        <br></br>
+       <button type="click" onClick={printDetails}
+        >Login</button>
+   </>
   )
 }
 
 export default Login
+    
