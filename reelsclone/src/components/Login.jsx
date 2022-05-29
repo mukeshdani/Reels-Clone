e1356b94b7e6a0d86c13a4d63a3b8396603d01d9
Mukesh Dani <mukeshdani00@gmail.com>
2022-05-29 12:57:37 +0530

    Login Auth

--- reelsclone/src/components/Login.jsx
+++ reelsclone/src/components/Login.jsx
@@ -1,28 +1,72 @@
 import React , { useState } from 'react'
+import {auth} from "../firebase";
+import { signInWithEmailAndPassword ,signOut} from "firebase/auth";
+
+
 
 function Login() {
-    let [email, setEmail] = useState("");
-    let [password, setPassword] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  let [user, setUser] = useState(null);
+  let [loader, setLoader] = useState(false);
+  let [error, setError] = useState("");
     const trackEmail = function (e) {
         setEmail(e.target.value);
       }
       const trackPassword = function (e) {
         setPassword(e.target.value);
       }
-    const printDetails = (e)=>{
+      const printDetails = async function () {
+       
       alert(email + " " + password);
+      try {
+        setLoader(true);
+        // signin call hoga 
+        let userCred = await
+          signInWithEmailAndPassword(auth, email, password)
+        // console.log(userCred.user);
+        setUser(userCred.user);
+      } catch (err) {
+        setError(err.message);
+        // after some time -> error message remove 
+        setTimeout(() => {
+          setError("")
+        }, 2000)
+      }
+      setLoader(false);
     }
+
+    const signout = async function () {
+      // ye call hoga 
+      await signOut(auth);
+      setUser(null);
+    }
+  
+
+
     return (
    <>
-      <input type="email" onChange={trackEmail} value={email} placeholder="email" ></input>
+   {  
+       error !== "" ?<h1>Error is {error}</h1>:
+       loader === true ? <h1> ...Loading </h1>:
+       user != null ? <>
+       <button
+                  onClick={signout}
+                >Signout</button>
+       </>:
+
+   <> <input type="email" onChange={trackEmail} value={email} placeholder="email" ></input>
         <br></br>
       <input type="password" onChange={trackPassword} value={password} placeholder="password"></input>
         <br></br>
        <button type="click" onClick={printDetails}
-        >Login</button>
+        >Login</button></>
+    }
    </>
   )
-}
+   }
+
 
 export default Login
     
---
+++ reelsclone/src/secrets.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCZQcYGc_qNFZmgBZ-m68oPowI1iLWJ8w",
+    authDomain: "reels-auth-b0382.firebaseapp.com",
+    projectId: "reels-auth-b0382",
+    storageBucket: "reels-auth-b0382.appspot.com",
+    messagingSenderId: "900515285677",
+    appId: "1:900515285677:web:2ce963afdaaf7b89a79424",
+    measurementId: "G-ZXDR4GNCDR"
+  };
+
+  module.exports=firebaseConfig;
